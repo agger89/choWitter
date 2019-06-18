28dbbfce07f4a108dc0b4fa84797d9934d933ea2
agger89 <agger2154@gmail.com>
2019-06-19 02:56:49 +0900

    ant design 적용

--- ch1/front/pages/index.js
+++ ch1/front/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Link from 'next/link'; // 라우팅
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello next</div>
+            <Head>
+                <title>choWitter</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.css" />
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>hello next</div>
+            </AppLayout>
         </>
     );
 };
