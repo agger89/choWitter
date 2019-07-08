ca0d8a5a20caf744183f57451985e8e26df864da
agger89 <agger2154@gmail.com>
2019-07-09 02:14:32 +0900

    회원가입 컨트롤러 만들기

--- ch1/back/index.js
+++ ch1/back/index.js
@@ -8,7 +8,12 @@ const postsAPIRouter = require('./routes/posts');
 const app = express();
 db.sequelize.sync();
 
-// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
+// json 데이터 형식 처리
+app.use(express.json());
+// form 데이터 형식 처리
+app.use(express.urlencoded({ extended: true }));
+
+// API: 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
 app.use('/api/posts', postsAPIRouter);
--- ch1/back/routes/user.js
+++ ch1/back/routes/user.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../models');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-
-});
+router.post('/', async (req, res, next) => { // POST /api/user 회원가입
+    try {
+      // 유저가 이미 있는지 확인
+      const exUser = await db.User.findOne({
+        where: {
+          // req.body.userId: front에서 요청보낸 유저 id
+          userId: req.body.userId,
+        },
+      });
+      if (exUser) {
+        return res.status(403).send('이미 사용중인 아이디입니다.');
+      }
+      // bcrypt 암호화
+      // salt는 10~13 사이로
+      const hashedPassword = await bcrypt.hash(req.body.password, 12); 
+      // 새로운 유저 생성 후 db에 저장
+      const newUser = await db.User.create({
+        nickname: req.body.nickname,
+        userId: req.body.userId,
+        password: hashedPassword,
+      });
+      console.log(newUser);
+      // front로 새로운 유저 응답
+      return res.status(200).json(newUser);
+    } catch (e) {
+      console.error(e);
+      // 에러 처리를 여기서
+      return next(e);
+    }
+  });
 
 router.get('/:id', (req, res) => {
 
