9d2c6019462e0ea62f463edf10020eb3a41d1cd7
agger89 <agger2154@gmail.com>
2019-08-03 02:29:14 +0900

    lambda code 추가

--- ch1/lambda/index.js
+++ ch1/lambda/index.js
@@ -2,14 +2,40 @@ const AWS = require('aws-sdk');
 const Sharp = require('sharp');
 
 const S3 = new AWS.S3({ region: 'ap-northeast-2' });
-
 exports.handler = async (event, context, callback) => {
-  // 버킷 이름 - react-starcho
   const Bucket = event.Records[0].s3.bucket.name;
-  // 파일 경로
   const Key = event.Records[0].s3.object.key;
-  // 파일명
-  const filename = Key.split('/')[[Key.split('/').length -1]];
-  // 확장자
-  const ext = Key.split('.')[[Key.split('.').length -1]];
+  const filename = Key.split('/')[Key.split('/').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1];
+  console.log(Key, filename, ext);
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
+
+  try {
+    const s3Object = await S3.getObject({ // S3에서 이미지를 받아 옵니다.
+      Bucket,
+      Key,
+    }).promise();
+    console.log('original', s3Object.Body.length);
+
+    const resizedImage = await Sharp(s3Object.Body)
+      .resize(800, 800, {
+        fit: 'inside',
+      })
+      .toFormat(requiredFormat)
+      .toBuffer();
+
+    console.log('resize', resizedImage.length);
+    await S3.putObject({
+      Body: resizedImage,
+      Bucket,
+      Key: `thumb/${filename}`,
+    }).promise();
+    console.log('put');
+
+    return callback(null, `thumb/${filename}`);
+    
+  } catch (error) {
+    console.error(error);
+    return callback(error);
+  }
 };
