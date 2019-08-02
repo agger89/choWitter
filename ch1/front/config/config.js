8f8cb201b6b06f03d95ff24138c8707bca22526d
agger89 <agger2154@gmail.com>
2019-08-02 20:08:50 +0900

    test

--- ch1/front/config/config.js
+++ ch1/front/config/config.js
@@ -1,5 +1,4 @@
 // 배포용 url과 개발용 url
-// const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.chostar.info' : 'http://localhost:3065';
-const backUrl = 'http://localhost:3065';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.chostar.info' : 'http://localhost:3065';
 
 export { backUrl };
